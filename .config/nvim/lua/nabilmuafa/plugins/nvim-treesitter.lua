19c371bab9366874f7594becda3b25e3f3ab55e5
nabilmuafa <109450293+nabilmuafa@users.noreply.github.com>
2026-04-28 15:00:07 +0700

    feat: upgrade treesitter & telescope

--- .config/nvim/lua/nabilmuafa/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/nabilmuafa/plugins/nvim-treesitter.lua
@@ -1,6 +1,8 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'gdscript', 'godot_resource', 'gdshader', 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
@@ -19,7 +21,7 @@ return {
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter').setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
